c85517f039bc98b1b4870bbe4029fbaeb536723f
Weslley Gatti <weslleygatti1@hotmail.com>
2026-03-26 20:08:23 -0300

    Primeiro commit com informações basicas do PRIMEIRO filme

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+        
+        int anoDeLancamento = 2022;
+        System.out.println("ano de lançamento: "+ anoDeLancamento);
+        boolean incluidoNoPlano = True;
+        doubl notaDoFilme = 8.1;
     }
 }
